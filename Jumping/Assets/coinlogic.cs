d544fa32f91966a3bb61c4943d2eb03cbd5da1e2
Dikshit Kaushal <47450978+dikshitkaushal@users.noreply.github.com>
2020-03-26 12:37:24 +0530

    movement_and_rotation_of_coins

---
+++ Jumping/Assets/coinlogic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinlogic : MonoBehaviour
+{
+    float speed = 2f;
+    Quaternion pos;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.left, 1f);
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
