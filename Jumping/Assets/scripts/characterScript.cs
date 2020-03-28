ff52f5f1913f0e08c151d6840be2807f544928d0
Dikshit Kaushal <47450978+dikshitkaushal@users.noreply.github.com>
2020-03-28 19:44:43 +0530

    bulletshooter

--- Jumping/Assets/scripts/characterScript.cs
+++ Jumping/Assets/scripts/characterScript.cs
@@ -8,7 +8,7 @@ public class characterScript : MonoBehaviour
     CharacterController m_charactercontroller;
     float m_horizontalaxis;
     float m_speed = 5f;
-    public float jumpd = 1f;
+    public float jumpd = 50f;
     float gravity = 0.05f;
     public bool isjumping = false;
     float m_verticalaxis;
@@ -26,22 +26,42 @@ public class characterScript : MonoBehaviour
     {
         m_horizontalaxis = Input.GetAxis("Horizontal");
         m_verticalaxis = Input.GetAxis("Vertical");
+        total_move = new Vector3(-m_verticalaxis, 0, m_horizontalaxis);
         Debug.Log(m_horizontalaxis);
         if(Input.GetButtonDown("Jump") && !isjumping)
         {
             isjumping = true;
-            m_movement.y = jumpd;
-        }
 
+        }
+        
+
+    }
+    public void Rotate_With_Mouse()
+    {
+        Vector3 mouse_screen_pos = Input.mousePosition;
+        Vector3 player_pos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 direction = mouse_screen_pos - player_pos;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(-angle, Vector3.up);
     }
     private void FixedUpdate()
     {
-        m_movement.x = m_horizontalaxis * m_speed * Time.deltaTime;
-        m_movement.z = m_verticalaxis * m_speed * Time.deltaTime;
+
+        m_movement = total_move * m_speed * Time.deltaTime;
+
+        /*        m_movement.x = m_horizontalaxis * m_speed * Time.deltaTime;
+                m_movement.z = m_verticalaxis * m_speed * Time.deltaTime;*/
+        /* if (total_move != Vector3.zero)
+         {
+             transform.forward = total_move.normalized;
+         }*/
+        Rotate_With_Mouse();
         if (m_charactercontroller)
         {
             m_charactercontroller.Move(m_movement);
         }
+        
         //controlling Gravity
         if (!m_charactercontroller.isGrounded)
         {
